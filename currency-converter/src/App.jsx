7d81f9a5b2c6fb6d3b4f2eda4e892d63248d5688
Myrone Silverio <silveriomyrone@gmail.com>
2023-08-15 00:25:43 +0800

    Styled to make it more presentable.

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import CurrencyRow from "./CurrencyRow";
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <CurrencyRow></CurrencyRow>
+      <p className="equals">=</p>
+      <CurrencyRow></CurrencyRow>
     </>
   );
 }
---
+++ currency-converter/src/CurrencyRow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CurrencyRow() {
+  return (
+    <>
+      <input type="number" className="input" />
+      <select value="HI">
+        <option value="HI">HI</option>
+      </select>
+    </>
+  );
+}
