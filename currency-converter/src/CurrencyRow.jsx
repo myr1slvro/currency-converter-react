0361995ddbaf620cc9673b628673048e77f8d4f6
Myrone Silverio <silveriomyrone@gmail.com>
2023-08-15 02:49:19 +0800

    Finished all the functionalities needed.

--- currency-converter/src/CurrencyRow.jsx
+++ currency-converter/src/CurrencyRow.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount,
+  } = props;
   return (
     <>
-      <input type="number" className="input" />
-      <select value="HI">
-        <option value="HI">HI</option>
+      <input
+        type="number"
+        className="input"
+        value={isNaN(amount) ? "" : amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </>
   );
